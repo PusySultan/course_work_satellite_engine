2d09b62330000574052c008e91226eb71ae1d595
DESKTOP-KLGCVKK\Kirill <kirillsoloduhin072@gmail.com>
2026-05-12 22:08:54 +0300

    Добавил аннотацию

--- engine/src/main/java/org/example/repositories/AntennaRepository.java
+++ engine/src/main/java/org/example/repositories/AntennaRepository.java
@@ -2,7 +2,9 @@ package org.example.repositories;
 
 import org.example.dbModels.Antenna;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AntennaRepository extends JpaRepository<Antenna, Integer>
 {
 }
