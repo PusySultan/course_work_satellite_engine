da6697593b7fea0e28ccba176c13762bc09e88a4
DESKTOP-KLGCVKK\Kirill <kirillsoloduhin072@gmail.com>
2026-05-16 19:31:33 +0300

    Изменил выделение блока переопределений

--- engine/src/main/java/org/example/services/OverrideService.java
+++ engine/src/main/java/org/example/services/OverrideService.java
@@ -7,47 +7,15 @@ import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
 @Service
-public class OverrideService {
-    private final ObjectMapper mapper = new ObjectMapper();
-    private final String[] requiredFields = new String[]{
-            "antennaDiameter", "antennaEfficiency", "gainConverter"
-    };
+public class OverrideService extends BaseModelServiceClass
+{
 
-    public JsonNode getOverrideBlock(String jsonString)
+    public JsonNode getOverrideBlock(String topJsonStr)
     {
-        JsonNode topJson = getTopJson(jsonString);
-        JsonNode overrideBlock = getOverrideBlock(topJson);
+        /// Конвертируем String в JsonNode
+        JsonNode topJsonNode = super.mapBody(topJsonStr);
 
-        validateOverrideBlock(overrideBlock);
-
-        return overrideBlock;
-    }
-
-    private void validateOverrideBlock(JsonNode topJson)
-    {
-        for (String field : requiredFields)
-        {
-            if(!topJson.has(field)) {
-                throw new GlobalException("В блоке констант и переопределений, отсутствует поле " + field);
-            }
-        }
-    }
-
-    private JsonNode getOverrideBlock(JsonNode topJson)
-    {
-        if (!topJson.has("override")) {
-            throw new GlobalException("Отсутствует блок переопределений и констант");
-        }
-
-        return topJson.get("override");
-    }
-
-    private JsonNode getTopJson(String jsonString)
-    {
-        try {
-            return mapper.readTree(jsonString);
-        } catch (JacksonException e) {
-            throw new GlobalException("Ошибка преобразования блока верхнего уровня");
-        }
+        /// Получаем блок информации о блоке переопределений
+        return super.getRequiredBlockByName(topJsonNode, "override");
     }
 }
